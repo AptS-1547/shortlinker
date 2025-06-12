ba0a84b38c5fdf71dd43a66c90e84552551edb3b
AptS-1547 <esaps@esaps.net>
2025-06-13 03:00:53 +0800

    refactor: 删除前端文件夹

--- admin-panel/src/types/env.d.ts
+++
@@ -1,9 +0,0 @@
-interface ImportMetaEnv {
-  readonly VITE_API_BASE_URL: string
-  readonly VITE_ADMIN_ROUTE_PREFIX: string
-  readonly VITE_HEALTH_ROUTE_PREFIX: string
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-}
